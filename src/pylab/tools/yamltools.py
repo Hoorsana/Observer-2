8ff3bddd85f77a1679dcb991e93a3394f136e576
maltekliemann <mail@maltekliemann.com>
2021-04-20 18:12:18 +0200

    Fix nonlocal error in yamltools
    We fix the nonlocal error which defines the tag of ``yaml_safe_loader``
    on the first call by using an additional indirection and a trick from
    dataclasses to make the decorator callable with and without parens.

--- src/pylab/tools/yamltools.py
+++ src/pylab/tools/yamltools.py
@@ -3,36 +3,55 @@ from typing import Optional
 import yaml
 
 
-def yaml_object(loader, tag: Optional[str] = None):
-    """Create a decorator which turns a class into a yaml object.
+def yaml_object(_cls=None, loader=None, tag: Optional[str] = None):
+    """Turn class into a YAML object with tags and default loader.
 
     Args:
-        loader: The default loader of the object
+        _cls:
+            Sentinel parameter to check how ``yaml_object`` is called;
+            should _never_ be specified by keyword
+        loader: The loader of the object
         tag: The yaml tag of the object
 
-    The default yaml tag of ``class A`` is ``'!A'``.
+    The decorator may be called as ``yaml_object`` or as
+    ``yaml_object()``, i.e. with or without parens. The ``_cls``
+    parameter is used to detect which behavior is used and should
+    *never* be specified by keyword. (This trick is from the
+    ``dataclasses`` builtin module.)
+
+    If ``yaml_object`` is used, then default values are used for
+    ``loader`` and ``tag``: The default loader is ``yaml.SafeLoader``
+    and the default yaml tag of ``class A`` is ``'!A'``.
+
+    Note that the original class is modified when the decorator is
+    applied. Don't call ``B = yaml_object(A)`` and then reuse  ``A``!
     """
-    def inner(cls):
-        nonlocal tag
-        nonlocal loader
-        if tag is None:
-            tag = u'!' + cls.__name__
 
-        @classmethod
-        def from_yaml(cls, loader, node):
-            d = loader.construct_mapping(node, deep=True)
-            return cls(**d)
-        cls.from_yaml = from_yaml
-        cls.yaml_tag = tag
-        cls.yaml_loader = loader
+    def wrap(cls):
+        return _process_class(cls, loader, tag)
 
-        # Note that ``result`` must be created _after_ setting
-        # the ``from_yaml`` methods, the tag and the loader. Otherwise,
-        # the meta object will not register the constructor.
-        bases = (yaml.YAMLObject,) + inspect.getmro(cls)
-        result = type(cls.__name__, bases, cls.__dict__.copy())
-        return result
-    return inner
+    if _cls is None:
+        return wrap  # Called with parens.
+    return wrap(_cls)  # Called without parens.
 
 
-yaml_safe_object = yaml_object(loader=yaml.SafeLoader)
+def _process_class(cls, loader=None, tag: Optional[str] = None):
+    if loader is None:
+        loader = yaml.SafeLoader
+    if tag is None:
+        tag = u'!' + cls.__name__
+
+    @classmethod
+    def from_yaml(cls, loader, node):
+        d = loader.construct_mapping(node, deep=True)
+        return cls(**d)
+    cls.from_yaml = from_yaml
+    cls.yaml_tag = tag
+    cls.yaml_loader = loader
+
+    # Note that ``result`` must be created _after_ setting
+    # the ``from_yaml`` methods, the tag and the loader. Otherwise,
+    # the meta object will not register the constructor.
+    bases = (yaml.YAMLObject,) + inspect.getmro(cls)
+    result = type(cls.__name__, bases, cls.__dict__.copy())
+    return result
--- tests/tools/test_yamltools.py
+++ tests/tools/test_yamltools.py
@@ -6,13 +6,17 @@ from pylab.tools import yamltools
 
 @pytest.fixture
 def cls():
-    @yamltools.yaml_safe_object
+    @yamltools.yaml_object
     class A:
+        def __init__(self, x):
+            self.x = x
+    @yamltools.yaml_object
+    class B:
         def __init__(self, x):
             self.x = x
 
 
-def test_yaml_safe_object(cls):
+def test_yaml_object(cls):
     value = 123
     a = yaml.safe_load(
         f"""
