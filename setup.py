96843e9cc398707c87505674da8c40ea5d9f94aa
maltekliemann <mail@maltekliemann.com>
2021-04-28 11:53:46 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as readme:
 
 setuptools.setup(
     name='pylab',
-    version='0.1.5',
+    version='0.1.6',
     author='M. Kliemann',
     author_email='mail@maltekliemann.com',
     description='hardware test automation software',
