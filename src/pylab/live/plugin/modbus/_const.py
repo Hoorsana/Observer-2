cda9c5e137d8f31648855d1e89231c88f8126794
maltekliemann <mail@maltekliemann.com>
2021-09-16 13:40:23 +0200

    License updates

--- src/pylab/live/plugin/modbus/_const.py
+++ src/pylab/live/plugin/modbus/_const.py
@@ -1,4 +1,5 @@
-from __future__ import annotations
-
+# SPDX-FileCopyrightText: 2021 Forschungs- und Entwicklungszentrum Fachhochschule Kiel GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 DEFAULT_SLAVE = 0
